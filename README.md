f23220c0f3374481660cb82d89c624bc271fc6be
PabloA1A <pablo_abadalvarez@hotmail.es>
2024-09-07 21:17:44 +0200

    vitest installed, more coverage and configured. Created TodoCard component with its logic

--- README.md
+++ README.md
@@ -6,3 +6,22 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Project:
+TodoList is to have a list that we are adding and in turn these additions can be modified or deleted.
+
+Created with React. 
+
+### To start the project:
+1- npm install
+2- npm run dev
+
+### All the tests:
+npm install vitest --save-dev
+npm install --save-dev vitest @testing-library/react @testing-library/jest-dom
+npm install @vitest/coverage-v8 --save-dev
+
+### AUTHOR:
+```sh
+Pablo Abad
+```
---
+++ src/components/TodoCard.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+export default function TodoCard(props) {
+    const { children, handleDeleteTodo, index, handleEditTodo } = props
+    return (
+        <li className='todoItem'>
+            {children}
+            <div className='actionsContainer'>
+                <button onClick={() => handleEditTodo(index)}>
+                    <i className="fa-solid fa-pen-to-square"></i>
+                </button>
+                <button onClick={() => handleDeleteTodo(index)}>
+                    <i className="fa-regular fa-trash-can"></i>
+                </button>
+            </div>
+        </li>
+    )
+}
+
+TodoCard.propTypes = {
+    children: PropTypes.node.isRequired,
+    handleDeleteTodo: PropTypes.func.isRequired,
+    index: PropTypes.number.isRequired,
+    handleEditTodo: PropTypes.func.isRequired,
+}
